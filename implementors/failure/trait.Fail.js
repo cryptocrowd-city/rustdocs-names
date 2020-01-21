7345e95e6d2cbca12925afe444d453b554ab6243
Daniel Kraft <d@domob.eu>
2020-01-21 12:44:05 +0100

    Update to polished docs.
    Updates the docs to commit 0b342c6b365e6a7f7e867432e9a79257a00638b4, which
    in particular includes polishing to the rustdocs.

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {};
-implementors["bip39"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"bip39/enum.ErrorKind.html\" title=\"enum bip39::ErrorKind\">ErrorKind</a>",synthetic:false,types:["bip39::error::ErrorKind"]},];
+implementors["bip39"] = [{"text":"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"bip39/enum.ErrorKind.html\" title=\"enum bip39::ErrorKind\">ErrorKind</a>","synthetic":false,"types":["bip39::error::ErrorKind"]}];
 implementors["failure"] = [];
-implementors["schnorrkel"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"schnorrkel/errors/enum.SignatureError.html\" title=\"enum schnorrkel::errors::SignatureError\">SignatureError</a>",synthetic:false,types:["schnorrkel::errors::SignatureError"]},];
+implementors["schnorrkel"] = [{"text":"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"schnorrkel/errors/enum.SignatureError.html\" title=\"enum schnorrkel::errors::SignatureError\">SignatureError</a>","synthetic":false,"types":["schnorrkel::errors::SignatureError"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["frame_support"] = [{text:"impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"frame_support/debug/struct.RuntimeLogger.html\" title=\"struct frame_support::debug::RuntimeLogger\">RuntimeLogger</a>",synthetic:false,types:["frame_support::debug::RuntimeLogger"]},];
+implementors["frame_support"] = [{"text":"impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"frame_support/debug/struct.RuntimeLogger.html\" title=\"struct frame_support::debug::RuntimeLogger\">RuntimeLogger</a>","synthetic":false,"types":["frame_support::debug::RuntimeLogger"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
